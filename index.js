8c2052bfab03ff84c340145f5697461944c9ff71
qinsfu <44136546+qxdi@users.noreply.github.com>
2020-05-04 11:55:32 +0800

    Update index.js
    reduce dependence

--- index.js
+++ index.js
@@ -5,21 +5,12 @@
  * Licensed under the MIT license.
  */
 
-var isNumber = require('is-number');
-var slice = require('array-slice');
-
 module.exports = function arrayFirst(arr, num) {
   if (!Array.isArray(arr)) {
     throw new Error('array-first expects an array as the first argument.');
   }
-
-  if (arr.length === 0) {
-    return null;
-  }
-
-  var first = slice(arr, 0, isNumber(num) ? +num : 1);
-  if (+num === 1 || num == null) {
-    return first[0];
-  }
-  return first;
+  
+  if(isNaN(+num)) num = 1;
+  
+  return Array(num).fill(0).map((i, index) => arr[index]);
 };
